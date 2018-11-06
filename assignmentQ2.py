e65b9e8228c19c36d129f2433ceac8c0bf1dbf77
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-06 15:36:27 +0000

    First attempt to answer 2(d)

--- assignmentQ2.py
+++ assignmentQ2.py
@@ -55,15 +55,79 @@ def detLU(matrix):
     return product
 
 
-""" Answer question 2
+""" Solve matrix equation LUx = b for vectors x,b
+    (Note that here the original matrix is overwritten as part of the function)
 """
+def solveEquation(L, U, b):
+    
+    #Forward substitution to solve for y (from Ly=b)
+    y = []
+    y.append( b[0] / L[0][0] )
+    
+    for i in range(1,len(b)):               # i=1, 2, 3, 4 for our b
+        sumY = 0
+        for j in range(i-1):
+            sumY += L[i][j] * y[j]
+        y.append( 1./L[i][i] * (b[i] - sumY) )
+#    print('y =', y)
+        
+    #Backward substitution to solve for x (from Ux=y)
+    x = []
+    x.insert(0, (y[len(b)-1] / U[len(b)-1][len(b)-1] ))
+    
+    for i in range( (len(b)-2), -1, -1 ):   # i=3, 2, 1, 0 for our b
+        sumX = 0
+        for j in range(i,len(b)):
+#            print('x = ', x)
+#            print('(i, j) = (', i, ',',j, ')')
+#            print('U[i][j]=', U[i][j], 'and x[j]=', x[j]) 
+            sumX += U[i][j] * x[j-len(b)+1]
+        x.insert(0, ( 1./U[i][i] * (y[i] - sumX) ))
+    
+    return x
+        
 
+
+""" Answer question 2 (b) 
+"""
 matrixA = np.array([[3., 1., 0., 0., 0.],
                    [3., 9., 4., 0., 0.],
                    [0., 9., 20., 10., 0.],
                    [0., 0., -22., 31., -25.],
                    [0., 0., 0., -55., 60.]])
- 
-print('L*U = \n', decompose(matrixA))
-#print('matrixA = ', matrixA)
-print('det(A) = ', detLU(matrixA))
+
+lu = decompose(matrixA)
+determinant = detA(lu)
+#print('L*U = \n', lu)
+#print('det(A) = ', determinant
+
+
+""" Answer question 2 (d) 
+"""
+#LOWER
+matrixL = np.identity(len(lu))
+for i in range(len(lu)):
+    matrixL[i][0:i] = lu[i][0:i]        #Keep 1s on diagonal 
+
+#UPPER
+matrixU = np.identity(len(lu))
+for i in range(len(lu)):
+    matrixU[i][i:] = lu[i][i:]
+
+#print('matrixL = \n', matrixL)
+#print('matrixU = \n', matrixU) 
+
+bVector = np.array([2., 5., -4., 8., 9.])
+xVector = solveEquation( matrixL, matrixU, bVector )
+
+print('x = \n', xVector)
+#x = [-0.26810222024018593, 
+#       1.0999144334855182, 
+#       -0.49543663973599689, 
+#       0.041044412500957146, 
+#       0.30430795221431156]
+
+
+""" Answer question 2 (e)
+"""
+
