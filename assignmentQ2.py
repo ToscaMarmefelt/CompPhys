f06c08f5e26e8d2c19bb2e30e55bed10c154dc58
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-06 13:45:16 +0000

    Cleaned up code. Correct answers for 2 (a) & (b)

--- assignmentQ2.py
+++ assignmentQ2.py
@@ -8,33 +8,12 @@ Created on Sat Nov  3 15:37:00 2018
 Carry out LU decomposition of an arbitrary NxN matrix using Crout's method.
 """
 import numpy as np 
-import random
 
 
 """ Use Crout's method for decomposing a square matrix A into
     an upper and a lower matrix.
 """
-#def decomposeNew(A):
-#    luMatrix = np.empty( (len(A),len(A)), int)
-#    
-#    for j in range(len(A)):
-#        #luMatrix[0][j] = A[0][j]    # Beta[1][j] is just A[0][j] bc no sum
-#        
-#        for i in range(j):           # Calculate all Beta values for given j
-#            mySumB = 0
-#            for k in range(i-1):
-#                mySumB += luMatrix[i][k] * luMatrix[k][j]
-#            luMatrix[i][j] = ( A[i][j] - mySumB )
-#            
-#        for i in range((j+1),len(A)):  # Calculate all Alpha values for given j
-#            mySumA = 0
-#            for k in range(j-1):
-#                mySumA += luMatrix[i][k] * luMatrix[k][j]
-#            luMatrix[i][j] = ( 1/luMatrix[j][j] * (A[i][j] -mySumA ))  
-#    return luMatrix
 
-
-#Alternative method where we just place new values in original matrix
 def decompose(myMatrix):
     
     for j in range(len(myMatrix)):   # For each column j
@@ -66,37 +45,25 @@ def decompose(myMatrix):
     
     return myMatrix
 
-# Calculate the determinant of a square matrix A
-def detA(myMatrix):
-#    decompose(myMatrix)
+# Calculate the determinant of a square (already decomposed) matrix A=L*U
+def detLU(matrix):      
     
     product = 1
-    for n in range(len(myMatrix)):
-        product *= myMatrix[n][n]
+    for n in range(len(matrix)):
+        product *= matrix[n][n]
     
     return product
 
 
-""" Generate a random NxN matrix
-    Returns an array of N lists, each with N entries.
+""" Answer question 2
 """
-#myMatrix = []
-#N = 3                       # random.randint(0,10) if we do not wish to set N
-#
-#for i in range(N**2):
-#    myMatrix.append(random.randint(0,10))
-#myMatrix = np.reshape(myMatrix, (N,N))
 
-""" Test code
-"""
 matrixA = np.array([[3., 1., 0., 0., 0.],
                    [3., 9., 4., 0., 0.],
                    [0., 9., 20., 10., 0.],
                    [0., 0., -22., 31., -25.],
                    [0., 0., 0., -55., 60.]])
  
-#print('A = ', matrixA)
 print('L*U = \n', decompose(matrixA))
-#lu = decompose(matrixA)
-#print(lu) 
-#print('det A = ', detA(matrixA)) 
+#print('matrixA = ', matrixA)
+print('det(A) = ', detLU(matrixA))
