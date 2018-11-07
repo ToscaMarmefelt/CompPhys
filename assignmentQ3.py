7ce40f2504544d81023d8d75bb483fa473a84aa0
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-07 20:30:59 +0000

    Cubic spline interpolation for simple test data works

--- assignmentQ3.py
+++ assignmentQ3.py
@@ -60,8 +60,42 @@ def linearInterpolation(xList, xRange, fList):
 # Cubic spline interpolation
 # =============================================================================
 
-#from assignmentQ2 import decompose, solveEquation
+from assignmentQ2 import decompose, makeL, makeU, solveEquation
 
+#From lists of data points xList and yList, generate one row of matrix for each i (from 1 to n-1)
+
+def cubicSpline(xList, yList):
+    """
+    Implement Equation 4.15 to generate a matrix equation. Solve this using the routine
+    from Q2 to generate functions on the intervals. 
+    
+    xList:      List of original data points in x
+    yList:      List of original data points in y
+    """
+    matrix = []
+    bVector = []
+    
+    #CREATE MATRIX AND VECTOR
+    for i in range(1, len(xList)-1):
+#        print('i = ', i)
+        a_i = [0] * (len(xList))  
+#        print('1: a_i = ', a_i)
+        a_i[i-1], a_i[i], a_i[i+1] = (xList[i]-xList[i-1])/6, (xList[i+1]-xList[i-1])/3, (xList[i+1]-xList[i])/6
+#        print('2: a_i = ', a_i)
+        b_i = ( (yList[i+1]-yList[i]) / (xList[i+1]-xList[i-1]) ) - ( (yList[i]-yList[i-1]) / (xList[i]-xList[i-1]) )
+#        print('1: b_i = ', b_i)
+        
+        matrix.append(a_i[1:-1])
+        bVector.append(b_i)
+    
+#    bVector = bVector[1:-1] 
+#    print('matrix = ', matrix)
+    
+    #SOLVE MATRIX EQUATION FOR SECOND DERIVATIVE F
+    lu = decompose(matrix)
+    f2Vector = solveEquation( makeL(lu), makeU(lu), bVector )
+    
+    return f2Vector 
 
 
 
@@ -69,22 +103,23 @@ def linearInterpolation(xList, xRange, fList):
 # Perform linear interpolation on set of data 
 # =============================================================================
 
-xTest = [1., 2., 4., 7.]
-yTest = [10., 5., 3., 9.]
-plt.scatter(xTest, yTest)
+xTest = [1., 2., 4., 7., 8., 11.]
+yTest = [10., 5., 3., 9., 3., 5.]
+#plt.scatter(xTest, yTest)
 
+print( cubicSpline(xTest, yTest) )
 
-xRange =  np.linspace(1.,8.,100) #10 data points in the range 0-10 (including 10)
-interFuncs = linearFunction(xTest, yTest)
-xValues = linearInterpolation(xTest, xRange, interFuncs)[0]
-yValues = linearInterpolation(xTest, xRange, interFuncs)[1]
+#xRange =  np.linspace(1.,8.,100) #10 data points in the range 0-10 (including 10)
+#interFuncs = linearFunction(xTest, yTest)
+#xValues = linearInterpolation(xTest, xRange, interFuncs)[0]
+#yValues = linearInterpolation(xTest, xRange, interFuncs)[1]
 
 #print('fList = ', interFuncs)
 #print('xValues = ', xValues)
 #print('yValues = ', yValues)
 
-plt.plot(xValues, yValues)
-plt.show()
+#plt.plot(xValues, yValues, 'r')
+#plt.show()
 
         
         
