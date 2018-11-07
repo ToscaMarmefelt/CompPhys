348481dbebfc15280f0df757c5d1d77ccd17d65d
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-07 14:54:30 +0000

    Linear interpolation works

--- assignmentQ3.py
+++ assignmentQ3.py
@@ -10,7 +10,9 @@ Question 3: Interpolation
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# =============================================================================
+# Linear interpolation
+# =============================================================================
 
 def linearFunction(xList, yList):
 #   Define a linear function {f_i(x) = k_i*x + m_i} between every set of points i and i+1
@@ -24,21 +26,44 @@ def linearFunction(xList, yList):
     return f_i
     
 
-
 def linearInterpolation(xList, xRange, fList):
-#   To plot interpolation function for entire range of x (every value in xRange),
-#   we must determine what function i to call for each value
+    """ 
+    To plot interpolation function for entire range of x (every value in xRange),
+    we must determine what function i to call for each value
     
+    xList:      List of original data points in x
+    xRange:     List of x for which we want to plot the interpolated function
+    fList:      List of functions over each defined interval 
+    """ 
+    interpX = []
     interpY = []
-    for i in range(len(xList)-1):
-        print('i = ', i)
-        for n in range(len(xRange)-1):
-            print('n = ', n)
-            print( 'xRange[n] = ', xRange[n], ' and xList[i] = ', xList[i])
-            while xRange[n] <= xList[i]:
-                interpY.append( fList[i][0]*xRange[n] + fList[i][1] )
+    n = 0
     
-    return interpY
+    for i in range(1,len(xList)):
+#        print('i = ', i)
+#        for n in range(len(xRange)-1):
+#            print('n = ', n)
+#            print( 'xRange[n] = ', xRange[n], ' and xList[i] = ', xList[i])
+        
+        while xRange[n] <= xList[i]:
+#            print('n = ', n)
+#            print( 'xRange[n] = ', xRange[n], ' and xList[i] = ', xList[i])
+            interpX.append( xRange[n] )
+            interpY.append( fList[i-1][0]*xRange[n] + fList[i-1][1] )
+#            print('interpY = ', interpY)
+            n += 1
+    
+    return interpX, interpY
+
+
+# =============================================================================
+# Cubic spline interpolation
+# =============================================================================
+
+#from assignmentQ2 import decompose, solveEquation
+
+
+
 
 # =============================================================================
 # Perform linear interpolation on set of data 
@@ -49,11 +74,16 @@ yTest = [10., 5., 3., 9.]
 plt.scatter(xTest, yTest)
 
 
-xRange =  np.linspace(1.,7.,10) #10 data points in the range 0-10 (including 10)
+xRange =  np.linspace(1.,8.,100) #10 data points in the range 0-10 (including 10)
 interFuncs = linearFunction(xTest, yTest)
-yValues = linearInterpolation(xTest, xRange, interFuncs)
+xValues = linearInterpolation(xTest, xRange, interFuncs)[0]
+yValues = linearInterpolation(xTest, xRange, interFuncs)[1]
 
-plt.scatter(xRange, yValues, 'r')
+#print('fList = ', interFuncs)
+#print('xValues = ', xValues)
+#print('yValues = ', yValues)
+
+plt.plot(xValues, yValues)
 plt.show()
 
         
