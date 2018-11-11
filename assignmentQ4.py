2f03aa518f302dbd2d05fcf4b8389eb71f2149e8
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-11 19:53:24 +0000

    Probably works

--- assignmentQ4.py
+++ assignmentQ4.py
@@ -14,7 +14,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 N = 2**6                    #Sample size
-t = np.linspace(0, 8, N)    #
+t = np.linspace(-8, 8, N)    #N equally spaced samples on a time domain of length T = N(t[n+1]-t[n])
 
 def h(t):
     if t >= 3 and t <= 5:
@@ -22,18 +22,20 @@ def h(t):
     else:
         return 0
 
-h_signal = np.array([h(t_i) for t_i in t])
-g_response = np.array([ (1/(2*np.pi)) * np.e**(- (t_i - 4)**2 /2) for t_i in t])
+h_signal = np.array([h(t_n) for t_n in t])
+g_response = np.array([ (1/(2*np.pi)) * np.e**(- (t_n-4)**2 /2) for t_n in t])
 
 # FOURIER TRANSFORM h(t) and g(t)
 
 h_fft = np.fft.fft(h_signal)
-g_fft = np.fft.fft(g_response)
+#g_fft = np.fft.fftshift ( np.fft.fft(g_response) )
+g_fft = np.abs( np.fft.fft(g_response) )
 
-# By the convolution theorem:
+# By the Convolution Theorem:
 
 g_conv_h = np.fft.ifft( h_fft * g_fft )
 
 plt.plot(t, h_signal, 'r')
 plt.plot(t, g_response, 'b')
 plt.plot(t, g_conv_h, 'g')
+
