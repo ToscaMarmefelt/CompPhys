b54a2c6d1de38c8e1937df491b50e9d4897bbe2a
ToscaMarmefelt <43926378+ToscaMarmefelt@users.noreply.github.com>
2018-11-11 19:53:55 +0000

    Not done yet

--- assignmentQ5.py
+++ assignmentQ5.py
@@ -21,14 +21,14 @@ x = []
 for i in range(10**5):
     x.append(random.random())
 
-ap.hist(x, bins='freedman')
+#ap.hist(x, bins='freedman')
 
 
 # =============================================================================
 # Transformation method
 # =============================================================================
 yTrans = [np.arccos( 1-2*xn ) for xn in x]
-ap.hist(yTrans, bins='freedman') 
+#ap.hist(yTrans, bins='freedman') 
 
 
 # =============================================================================
@@ -36,3 +36,39 @@ ap.hist(yTrans, bins='freedman')
 # =============================================================================
 
 
+
+#def reject(yList):
+#    #Collect all y-values which are accepted by the rejection method
+#    yReject = []
+#    
+#    for yi in yList:
+#        
+#        #Choose p_i by applying transformation method to f(y)=2/np.pi * np.sin(y) 
+#        p_i = np.arccos( 1 - 2*yi )
+#        if yi < p_i:
+#            yReject.append(yi)
+##    print('yReject = ', yReject)
+#    return yReject
+
+
+# Define desired probability distribution function
+def P(y):
+    return (2/np.pi)*(np.sin(y))**2
+
+
+def reject(samplesWanted):
+    #Collect all y-values which are accepted by the rejection method into a list
+    yReject = []
+    
+    while len(yReject) < samplesWanted:
+        yi = np.arccos(1 - 2*random.random())
+        #Choose p_i by applying transformation method to f(y)=2/np.pi * np.sin(y)
+        p_i = np.arccos(1 - 2*yi) * random.random()
+        
+        if P(yi) >= p_i:
+            yReject.append(yi)
+    return yReject
+
+
+N = 10**5
+ap.hist(reject(N), bins='freedman')
